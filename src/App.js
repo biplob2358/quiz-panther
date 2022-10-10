d0f2efefe1b1bca785d1d147b26491eb948118cb
unknown <biplob2358@gmail.com>
2022-10-10 20:54:15 +0600

    daisyUi setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1 className="text-red-600">Hello</h1>
+      <h1 className="text-red-600 text-center">Hello</h1>
+      <button className="btn btn-primary ml-10">Button</button>
     </div>
   );
 }

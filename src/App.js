0701bac129f056bed47e1422537602ada0c72acf
unknown <biplob2358@gmail.com>
2022-10-11 00:35:29 +0600

    router  added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import About from "./components/About";
+import Blogs from "./components/Blogs";
 import ErrorPage from "./components/ErrorPage";
 import Home from "./components/Home";
-import Navbar from "./components/Navbar";
+
+import Root from "./components/Root";
+import Statistics from "./components/Statistics";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
+    element: <Root></Root>,
     errorElement: <ErrorPage></ErrorPage>,
-  },
-  {
-    path: "/home",
-    element: <Home></Home>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>,
+      },
+
+      {
+        path: "/blogs",
+        element: <Blogs></Blogs>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+    ],
   },
 ]);
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
